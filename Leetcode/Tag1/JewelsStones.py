e9aeff67dfaa7f7c0deaa99f732226d6172401ab
AnnVarsh <v.anubha7@gmail.com>
2019-02-24 14:56:17 -0500

    .

---
+++ Leetcode/Tag1/JewelsStones.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        # count = 0
+        # for i in S:
+        #     if i in J:
+        #         count+=1
+        # return count
+        
+        return sum(j in set(J) for j in S)
